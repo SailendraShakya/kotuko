c04e4c1ea259fa5ae0ca2c39b734b26f5eeacc74
Sailendra Shakya <sailendrashakya@Sailendras-MBP.domain.name>
2022-02-01 14:15:25 +0545

    request api according to section and query string from url

--- app/controllers/guardian_controller.rb
+++ app/controllers/guardian_controller.rb
@@ -1,4 +1,23 @@
 class GuardianController < ApplicationController
   def index
+    
   end
+
+  def search
+    section = 'politics/politics'
+    query = 'debate'
+
+    if(params.has_key?(:query))
+      query = params[:query]
+    end
+
+    if(params.has_key?(:section))
+      section = params[:section]
+    end
+
+    response = RestClient.get  "https://content.guardianapis.com/search?q="+query+"&tag="+section+"&from-date=2014-01-01&api-key=9d97b471-ee1c-473a-b293-7998a92c4182"
+    json = JSON.parse response
+    render :xml => json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root "guardian#index"
   get 'guardian/index'
+  get 'guardian/search', to: "guardian#search"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
